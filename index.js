3ef063705b5cdcc8f9a1eb0568b081ccc4c49a41
michaelrhodes <spam@michaelrhod.es>
2019-08-14 11:36:40 +1000

    Fix small size bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = length
 
 function length (bytes) {
-  return Math.max(16384, 1 << Math.log2(bytes / 1024) + 0.5 | 0)
+  return Math.max(16384, 1 << Math.log2(Math.max(1024, bytes / 1024)) + 0.5 | 0)
 }
